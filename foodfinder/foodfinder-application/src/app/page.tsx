a5dcbb11a379082f099f64b3b02b6ce37ce010b5
oscar066 <oscarkaruga1@gmail.com>
2024-08-29 01:21:36 +0300

    :lipstick: customized the Hello from finder using tailwindcss

--- foodfinder/foodfinder-application/src/app/page.tsx
+++ foodfinder/foodfinder-application/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="font-serif pt-6 text-center text-blue-800">
       <h1> Hello From FoodFinder. </h1>
     </div>
   );
