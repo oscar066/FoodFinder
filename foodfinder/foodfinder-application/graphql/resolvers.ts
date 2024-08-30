bc6ddfffc95c22aaf6d9def64f0f5a2f7678c209
oscar066 <oscarkaruga1@gmail.com>
2024-08-30 16:21:48 +0300

    :construction: graphql

---
+++ foodfinder/foodfinder-application/graphql/resolvers.ts
@@ -0,0 +1,14 @@
+
+import { locationMutations } from "./locations/mutations";
+import { locationQueries } from "./locations/queries";
+
+export const resolvers = {
+    Query: {
+        ...locationQueries,
+    },
+
+    Mutation: {
+        ...locationMutations,
+    },
+
+};
