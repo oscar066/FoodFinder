02770ea3f4231b4e18805352b41ead3bfb94755e
oscar066 <oscarkaruga1@gmail.com>
2024-09-07 21:58:01 +0300

    :construction: Authentication with next Auth and github

---
+++ foodfinder/foodfinder-application/src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,34 @@
+import GithubProviders from "next-auth/providers/github";
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth";
+import { createHash } from "crypto";
+
+const createUserId = (base: string): string => {
+    return createHash("sha256").update(base).digest("hex");
+};
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+    return await NextAuth(req, res ,{
+        providers: [
+            GithubProviders({
+                clientId: process.env.GITHUB_CLIENT_ID || " ",
+                clientSecret: process.env.GITHUB_CLIENT_SECRET || " ",
+            }),
+        ],
+        callbacks: {
+            async jwt({ token }){
+                if (token?.email && !token.fdlist_private_userId){
+                    token.fdlist_private_userId = createUserId(token.email);
+                }
+                return token;
+            },
+
+            async session({ session }){
+                if (session?.user?.email && !session?.user.fdlist_private_userId){
+                    session.user.fdlist_private_userId = createUserId(session?.user?.email);
+                }
+                return session;
+            }
+        }
+    })
+}
